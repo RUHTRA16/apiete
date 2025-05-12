e58a484b20ede7aae6c5d428ac95f5c0bd206eec
Emerson Cavalcanti <emerson.cavalcanti@escolatecnicalimoeiro.com.br>
2025-05-12 19:47:46 -0300

    add metodo para validacao no Request

--- app/Http/Requests/StoreProdutoRequest.php
+++ app/Http/Requests/StoreProdutoRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class StoreProdutoRequest extends FormRequest
 {
@@ -13,6 +15,19 @@ class StoreProdutoRequest extends FormRequest
     {
         return true;
     }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json(
+            [
+                'status' => false,
+                'message' => 'Validation errors',
+                'errors' => $validator->errors()
+            ],
+            422),
+        );
+    }
+
 
     /**
      * Get the validation rules that apply to the request.
