54f5c44d13c645707620599be50e05fc0bd63ca4
David Guilherme <davidguilherme12345@gmail.com>
2025-05-28 15:10:03 -0300

    imports necessarios

--- app/Models/Nota.php
+++ app/Models/Nota.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
 use Illuminate\Database\Eloquent\Model;
 
 class Nota extends Model
@@ -15,4 +16,3 @@ class Nota extends Model
         return $this->belongsTo(Aluno::class);
     }
 }
-
--- routes/api.php
+++ routes/api.php
@@ -10,14 +10,14 @@ use Illuminate\Support\Facades\Route;
 Route::post('/registro', [UserController::class, 'registro']);
 Route::post('/login', [UserController::class, 'login']);
 
-Route::post('/logout', [UserController::class, 'logout']);
-
-Route::apiResource('/produtos', ProdutoController::class);
-Route::apiResource('/alunos', AlunoController::class);
-
-Route::apiResource('alunos.notas', NotaController::class)->shallow();
-Route::apiResource('alunos.frequencias', FrequenciaController::class)->shallow();
 Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [UserController::class, 'logout']);
+    
+    Route::apiResource('/produtos', ProdutoController::class);
+    Route::apiResource('/alunos', AlunoController::class);
+    
+    Route::apiResource('alunos.notas', NotaController::class)->shallow();
+    Route::apiResource('alunos.frequencias', FrequenciaController::class)->shallow();
 });
 
 
